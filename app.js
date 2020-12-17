2f20191548bbddbf12f1fc1cf3d155065e71a6bd
khustaransari <66967063+khustaransari@users.noreply.github.com>
2020-12-17 22:39:26 +0530

    start

--- app.js
+++ app.js
@@ -236,7 +236,12 @@ app.get("/about",function(req,res)
 {
  res.render("about");
 });
-app.listen(3000,function()
+let port=process.env.PORT;
+if(port==null || port=="")
+{
+  port=3000;
+}
+app.listen(port,function()
 {
     console.log("Server running on 3000");
 })
